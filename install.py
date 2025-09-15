86f459b77451e5f160d25708ecc2d9dabfd8375a
Dayimshah <76784592+Dayimshah@users.noreply.github.com>
2025-09-16 02:22:51 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,8 @@
+#!pip install torch-geometric==2.0.1
+import torch
+
+!pip uninstall torch-scatter torch-sparse torch-geometric torch-cluster  --y
+!pip install torch-scatter -f https://data.pyg.org/whl/torch-{torch.__version__}.html
+!pip install torch-sparse -f https://data.pyg.org/whl/torch-{torch.__version__}.html
+!pip install torch-cluster -f https://data.pyg.org/whl/torch-{torch.__version__}.html
+!pip install git+https://github.com/pyg-team/pytorch_geometric.git
